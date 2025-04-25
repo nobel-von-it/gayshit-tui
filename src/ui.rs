60a8e30ca2082934e3495fc5d11da30472e78ac3
nobel-von-it <maksimdavydenko12@gmail.com>
2025-04-25 14:36:35 +0700

    add: ui functions

--- src/ui.rs
+++ src/ui.rs
@@ -1,9 +1,7 @@
-use std::borrow::Borrow;
-
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Direction, Layout, Rect},
-    style::Stylize,
+    style::{Style, Stylize},
     widgets::{Block, Paragraph, Widget},
 };
 
@@ -29,10 +27,36 @@ fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
         .split(popup_layout[1])[1] // Return the middle chunk
 }
 
+fn create_constraints_per(size: usize) -> Vec<Constraint> {
+    let mut constraints = Vec::with_capacity(size);
+    for _ in 0..size {
+        constraints.push(Constraint::Percentage(100 / size as u16));
+    }
+    constraints
+}
+
+fn create_constraints_len(size: usize, len: u16) -> Vec<Constraint> {
+    let mut constraints = Vec::with_capacity(size);
+    for _ in 0..size {
+        constraints.push(Constraint::Length(len));
+    }
+    constraints
+}
+
+fn create_choice(text: &str, selected: bool) -> Paragraph {
+    Paragraph::new(text)
+        .style(if selected {
+            Style::default().add_modifier(ratatui::style::Modifier::BOLD)
+        } else {
+            Style::default()
+        })
+        .block(Block::bordered())
+        .centered()
+}
+
 // fn render(self, area: Rect, buf: &mut Buffer)
 pub fn render_main_menu(choices: &[String], selected: usize, area: Rect, buf: &mut Buffer) {
     let menu_block = Block::bordered().title("Menu");
-    let choice_block = Block::bordered();
 
     let window_layout = centered_rect(90, 90, area);
     let subwindow_layout = centered_rect(95, 95, window_layout);
@@ -47,24 +71,10 @@ pub fn render_main_menu(choices: &[String], selected: usize, area: Rect, buf: &m
 
     let choices_layout = Layout::default()
         .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Length(3),
-            Constraint::Length(3),
-            Constraint::Length(3),
-        ])
+        .constraints(create_constraints_len(choices.len(), 3))
         .split(game_layout[0]);
     for (i, choice) in choices.iter().enumerate() {
-        if i == selected {
-            Paragraph::new(choice.as_str())
-                .centered()
-                .green()
-                .block(choice_block.clone())
-                .render(choices_layout[i], buf);
-        } else {
-            Paragraph::new(choice.as_str())
-                .centered()
-                .block(choice_block.clone())
-                .render(choices_layout[i], buf);
-        }
+        let text = &choice;
+        create_choice(text, i == selected).render(choices_layout[i], buf);
     }
 }
